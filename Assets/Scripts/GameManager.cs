ad7dd1eeb6f6577bc247dc7d7f4b1c1b4887f798
Daniel Legcevic <92600457+daniellegcevic@users.noreply.github.com>
2023-10-02 12:34:48 -0400

    Minor Changes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -78,7 +78,6 @@ public class GameManager : MonoBehaviour
             {
                 if(!sceneTransition)
                 {
-                    Debug.Log("Continue: " + continueGame);
                     if(continueGame)
                     {
                         LoadGameData();
