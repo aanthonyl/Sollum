3054c0c2ed9ee4e9d3d9279567f75779c5f35b08
Kayla England <englandkn16@gmail.com>
2023-10-05 19:51:28 -0400

    Enemy Melee script
    created and finished. needs testing and PlayerHealth

---
+++ Assets/Scripts/Enemies/EnemyMelee.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMelee : MonoBehaviour
+{
+    public int damage = 10;           
+    public float attackRange = 2.0f; 
+    public float attackCooldown = 2.0f; 
+
+    private Transform player; 
+    private float nextAttackTime; // Time when the next attack can occur.
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        // Check if the player is within attack range.
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        if (distanceToPlayer <= attackRange)
+        {
+            // Check if enough time has passed for the next attack.
+            if (Time.time >= nextAttackTime)
+            {
+                
+                AttackPlayer();
+
+                nextAttackTime = Time.time + attackCooldown;
+            }
+        }
+    }
+
+    private void AttackPlayer()
+    {
+        PlayerHealth playerHealth = player.GetComponent<PlayerHealth>();
+        if (playerHealth != null)
+        {
+            playerHealth.TakeDamage(damage);
+        }
+    }
+}
