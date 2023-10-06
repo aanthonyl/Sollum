53e4c3d0f46e8521134fc685d0857e5be51514b3
Kayla England <englandkn16@gmail.com>
2023-10-06 00:23:58 -0400

    edited script
    having issues, need to fix

--- Assets/Scripts/HealingZone.cs
+++ Assets/Scripts/HealingZone.cs
@@ -8,32 +8,39 @@ public class HealingZone : MonoBehaviour
     public float healAmount = 10.0f;
     private bool isHealing = false;
 
+    private PlayerHealth playerHealth;
+
     public AudioClip healingSound; 
-    private AudioSource audioSource;
+   // private AudioSource audioSource;
+    
 
 
     private void Start()
     {
-        audioSource = GetComponent<AudioSource>();
-        audioSource.clip = healingSound;
-        audioSource.loop = true; //loop sound while healing
+        // audioSource = GetComponent<AudioSource>();
+        // audioSource.clip = healingSound;
+        //audioSource.loop = true; //loop sound while healing
+
     }
     private void OnTriggerEnter(Collider col)
     {
         if(col.gameObject.CompareTag("Player"))
         {
             isHealing = true;
-            audioSource.Play();
+
+            Debug.Log("onTriggerEnter");
+
+            //audioSource.Play();
         }
 
     }
 
     private void OnTriggerExit(Collider col)
     {
-        if (col.gameObject.CompareTag("Player)"))
+        if (col.gameObject.CompareTag("Player)")) 
         {
             isHealing = false;
-            audioSource.Stop();
+            //audioSource.Stop();
         }
     }
 
@@ -41,7 +48,8 @@ public class HealingZone : MonoBehaviour
     {
         if (isHealing && col.CompareTag("Player"))
         {
-            // Heal the player over time.
+            Debug.Log("inside healing zone");
+
             PlayerHealth playerHealth = col.GetComponent<PlayerHealth>();
             if (playerHealth != null)
             {
