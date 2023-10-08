4f3a7ee71d87a26656c5ac3a5228c317cb3aa462
AuroraRBorealis <145414421+AuroraBorealis94@users.noreply.github.com>
2023-10-08 00:07:45 -0400

    Setting Up Pause UI and Scripts
    Added UI sprites, creating scripts, updating in my scene

---
+++ Assets/Scripts/PauseSystem/LocationLibrary.cs
@@ -0,0 +1,24 @@
+/*
+    Script Added by Aurora Russell
+	10/08/2023
+	// LOCATION LIBRARY MAKES SCRIPTABLE OBJECT OPTION //
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class LocationLibrary : MonoBehaviour
+{
+    // TO BE USED AS AN INPUT IN THE PauseManager.cs SCRIPT
+
+    public List<SpriteInfo> speakerSpriteList = new List<SpriteInfo>();
+
+    [System.Serializable]
+    public class SpriteInfo
+    {
+        public string name;
+        public Sprite sprite;
+    }
+}
--- Assets/Scripts/PauseSystem/PauseManager.cs
+++ Assets/Scripts/PauseSystem/PauseManager.cs
@@ -1,33 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // ALLOWS DEVELOPER TO SELECT KEY FROM LIST
+    [Header("Pause Key")]
+    public KeyCode PauseKey = KeyCode.Escape;
+
+    [Header("UI Elements")]
+    [Tooltip("Pause UI")]
+    public GameObject PauseUI;
+
+    //[Header("Save System")]
+    //[Tooltip("Save Script")]
+    //public SaveSystem saveSystem;
+
+    [Header("Scene Change")]
+    [Tooltip("Main Menu")]
+    public string levelToLoad;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // ADVANCE DIALOGUE USING SELECTED KEY FROM DialogueManager.cs //
+        if (Input.GetKeyDown(PauseKey))
+        {
+            Debug.Log("PAUSE GAME");
+            PauseUI.SetActive(true);
+        }
     }
 
     public void ContinueButton()
     {
-
+        Debug.Log("PAUSE GAME");
+        PauseUI.SetActive(false);
     }
 
     public void SaveButton()
     {
-
+        // Access save system
     }
 
     public void MainMenuButton()
     {
-
+        SceneManager.LoadScene(levelToLoad);
     }
 }
