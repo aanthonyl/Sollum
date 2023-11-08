90e4572f02609119073ea9bbfb08e71ab504ea38
Kayla England <englandkn16@gmail.com>
2023-11-07 22:46:27 -0500

    Created template for scenes, text and audio
    waiting on assets

--- Assets/Scripts/Dialogue/IntroCutScene.cs
+++ Assets/Scripts/Dialogue/IntroCutScene.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class IntroCutScene : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnEnable()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("Loading", LoadSceneMode.Single);
     }
 }
