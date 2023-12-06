906b73395ed5bdfba145878af73522b30be2af92
Jack <harolds_m@yahoo.com>
2023-12-06 18:33:06 -0500

    Sollum -- Decouple the arms from the mayor

--- Assets/Scripts/Bosses/Jack/arm_movement.cs
+++ Assets/Scripts/Bosses/Jack/arm_movement.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 
 public class arm_movement : MonoBehaviour
 {
-        private float max = 1.7f;
-        public float speed = .1f;
+        private float max = 13f;
+        public float speed = 2f;
 
         public float startPos;
         public bool forward = true;
@@ -28,36 +28,34 @@ public class arm_movement : MonoBehaviour
     {
         switch (armCenter._currState)
         {
-            switch(armCenter._currState)
-            {
-                case arm_controller.ArmState.Idle:
-                    // transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, Mathf.PingPong(Time.time*2,max-min)+min);
-                    if(forward)
-                        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z-speed*Time.deltaTime);
-                        // g = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z-speed*Time.deltaTime);
-                    else
-                        transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z+speed*Time.deltaTime);
-                        // g = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z+speed*Time.deltaTime);
+            case arm_controller.ArmState.Idle:
+                // transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, Mathf.PingPong(Time.time*2,max-min)+min);
+                if(forward)
+                    transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z-speed*Time.deltaTime);
+                    // g = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z-speed*Time.deltaTime);
+                else
+                    transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z+speed*Time.deltaTime);
+                    // g = new Vector3(transform.localPosition.x, transform.localPosition.y, transform.localPosition.z+speed*Time.deltaTime);
 
-                    if(Mathf.Abs(transform.localPosition.z-startPos) > max || transform.localPosition.z > startPos)
-                    // if(g.z < -max || transform.localPosition.z > 0f)
-                        forward = !forward;
-                    revUp = 0f;
-                    break;
-                case arm_controller.ArmState.Attack:
-                    // Debug.Log("Be Still");
-                    break;
-                case arm_controller.ArmState.Chase:
-                    // Lag Behind the Player
-                    revUp += rampUpScale;
-                    transform.localPosition = Vector3.Lerp(transform.localPosition, new Vector3(transform.localPosition.x, transform.localPosition.y, player.position.z), revUp);
-                    
-                    // g = Vector3.Lerp(transform.localPosition, new Vector3(transform.localPosition.x, transform.localPosition.y, player.localPosition.z), revUp);
-                    break;
-                default:
-                    // Debug.Log("Movement Default");
-                    break;
-            }
+                if(Mathf.Abs(transform.localPosition.z-startPos) > max || transform.localPosition.z > startPos)
+                // if(g.z < -max || transform.localPosition.z > 0f)
+                    forward = !forward;
+                revUp = 0f;
+                break;
+            case arm_controller.ArmState.Attack:
+                // Debug.Log("Be Still");
+                break;
+            case arm_controller.ArmState.Chase:
+                // Lag Behind the Player
+                revUp += rampUpScale;
+                transform.localPosition = Vector3.Lerp(transform.localPosition, new Vector3(transform.localPosition.x, transform.localPosition.y, player.position.z), revUp);
+                
+                // g = Vector3.Lerp(transform.localPosition, new Vector3(transform.localPosition.x, transform.localPosition.y, player.localPosition.z), revUp);
+                break;
+            default:
+                // Debug.Log("Movement Default");
+                break;
+        }
 
             // if(armCenter._currState == arm_controller.ArmState.Idle)
             // {
@@ -70,5 +68,5 @@ public class arm_movement : MonoBehaviour
             //     if(Mathf.Abs(transform.localPosition.z-startPos) > max || transform.localPosition.z > startPos)
             //         forward = !forward;
             // }
-        }
+    }
 }
